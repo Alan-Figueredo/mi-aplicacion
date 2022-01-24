087890ed021521f8bffa172ce780f7349e8560b0
Alan Figueredo <afigueredo2000@gmail.com>
2022-01-23 23:28:20 -0300

    cambios

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,14 +5,14 @@ import { ItemCount } from "../ItemCount"
 export const Item =({product})=>{
     return(
         <>
-        <div className="card mt-3 mx-3">
-            <div className="card-body">
-                <img className="imagen-card img-thumbnail" src={product.img}/>
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.description}</p>
-                <ItemCount stock={product.stock} initial={1}/>
+            <div className="card mt-3 mx-3">
+                <div className="card-body">
+                    <img className="imagen-card img-thumbnail" src={product.img}/>
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">{product.description}</p>
+                    <ItemCount stock={product.stock} initial={1}/>
+                </div>
             </div>
-        </div>
-    </>
+        </>
     );
 }
