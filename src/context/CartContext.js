423aad025312d40ba60404eeb0bd2b5a19a035b8
Alan Figueredo <afigueredo2000@gmail.com>
2022-02-10 06:34:37 -0300

    cambios

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,20 +4,32 @@ export const CartContext = createContext([]);
 
 export const CartProvider = ({ children })=>{
     const [cart, setCart] = useState([]);
-
+    const [cantidad, setCantidad] = useState(0)
     const addItem = (item, quantity) => {
         const newItem = { item, quantity };
         console.log("se agregó al carrito: ", newItem)
+        console.log(cart)
         setCart((prevState)=> [...prevState, newItem]);
+        setCantidad((prevState)=> prevState+ quantity);
     };
     const removeItem=(id)=>{
-        setCart((prev)=>prev.filter((element)=>element.item.id !== id))
+        setCart((prev)=>prev.filter((element)=>element.item.id !== id));
+        //setCantidad((prevState)=> prevState) como??
     }
     const clearAll=()=>{
         setCart([]);
+        setCantidad(0);
+    }
+    const calcularTotal=()=>{
+        const preTotal = cart.map(x=>x.price * x.quantity);
+        const total = 0;
+        for(let elemento of preTotal){
+            total = total + elemento;
+        }
+        return total;
     }
     return(
-        <CartContext.Provider value={{cart, addItem, removeItem, clearAll }}>
+        <CartContext.Provider value={{cart, addItem, removeItem, clearAll, calcularTotal, cantidad }}>
             {children}
         </CartContext.Provider>
     )
