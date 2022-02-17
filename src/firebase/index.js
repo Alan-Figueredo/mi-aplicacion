7d03d29487118db4877ba3e29845e7a3d2e1aea3
Alan Figueredo <afigueredo2000@gmail.com>
2022-02-17 12:27:34 -0300

    cambios

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCjkoi72r85GwHez7P-vLr6T3EcXVZOtGk",
-    authDomain: "tusbebidasonline.firebaseapp.com",
-    projectId: "tusbebidasonline",
-    storageBucket: "tusbebidasonline.appspot.com",
-    messagingSenderId: "942541020743",
-    appId: "1:942541020743:web:df4700435da497471e5b62"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   const app = firebase.initializeApp(firebaseConfig);
