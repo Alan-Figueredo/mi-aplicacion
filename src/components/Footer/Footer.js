ea188ba19bf5628cf4d898b72572f4c11a55ada5
Alan Figueredo <afigueredo2000@gmail.com>
2022-03-06 19:29:10 -0300

    cambios

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faWhatsapp, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import "../Footer/Footer.css"
 export const Footer =()=>{
-    let iconos={
-        fontSize:"25px", marginRight:"15px"
-    }
     return(
-            <footer className="bg-dark text-white mt-5" style={{marginTop:"auto", width:"100%"}}>
+            <footer className="bg-dark text-white mt-5" id="footer">
                 <div className="row">
                     <div className="col-sm-4">
-                        <h2 className="mt-4 mb-3" style={{marginLeft:"10px"}}>TusBebidasOnline</h2>
+                        <h2 className="mt-4 mb-3" id="tituloFooter">TusBebidasOnline</h2>
                     </div>
                     <div className="col-sm-4">
                         <h2 className="mt-4 mb-3">Contacto</h2>
-                        <FontAwesomeIcon icon={faInstagram} style={iconos} />
-                        <FontAwesomeIcon icon={faWhatsapp} style={iconos}/>
-                        <FontAwesomeIcon icon={faFacebook} style={iconos}/>
-                        <FontAwesomeIcon icon={faEnvelope} style={iconos}/>
+                        <FontAwesomeIcon icon={faInstagram} className="iconos" />
+                        <FontAwesomeIcon icon={faWhatsapp} className="iconos"/>
+                        <FontAwesomeIcon icon={faFacebook} className="iconos"/>
+                        <FontAwesomeIcon icon={faEnvelope} className="iconos"/>
                     </div>
                     <div className="col-sm-4">
                         <h2 className="mt-4 mb-3">Horarios</h2>
